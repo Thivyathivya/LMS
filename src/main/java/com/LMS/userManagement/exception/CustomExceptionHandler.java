26e08639266d44dc631686297722e6232ca247c2
sivakrays <siva.krays@gmail.com>
2024-01-16 16:38:19 +0530

    changes in SecurityConfiguration and edit profile by thivya

--- src/main/java/com/LMS/userManagement/exception/CustomExceptionHandler.java
+++ src/main/java/com/LMS/userManagement/exception/CustomExceptionHandler.java
@@ -13,11 +13,11 @@ public class CustomExceptionHandler {
     @ExceptionHandler(Exception.class)
     public ProblemDetail handleSecurityException(Exception ex){
         ProblemDetail errorDetail=null;
-        /*if (ex instanceof BadCredentialsException) {
+        if (ex instanceof BadCredentialsException) {
              errorDetail = ProblemDetail
                     .forStatusAndDetail(HttpStatusCode.valueOf(401), ex.getMessage());
             errorDetail.setProperty("access_denied_reason", "Authentication Failure");
-        }*/
+        }
         if (ex instanceof ExpiredJwtException) {
             errorDetail = ProblemDetail
                     .forStatusAndDetail(HttpStatusCode.valueOf(498), ex.getMessage());
